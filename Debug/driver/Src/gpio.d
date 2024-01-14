406ec02af8fdae437c1c93e747c6bf89c8b672c7
phamtien758 <phamtien758@gmail.com>
2024-01-14 20:27:39 +0700

    Remove unused file

--- Debug/driver/Src/gpio.d
+++
@@ -1,6 +0,0 @@
-driver/Src/gpio.o: ../driver/Src/gpio.c ../driver/Inc/gpio.h \
- ../driver/Inc/stm32f401re.h ../driver/Inc/type.h ../driver/Inc/stub.h
-../driver/Inc/gpio.h:
-../driver/Inc/stm32f401re.h:
-../driver/Inc/type.h:
-../driver/Inc/stub.h:
